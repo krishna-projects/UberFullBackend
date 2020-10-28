c194cc174841d5b8b28336aed8e0123c311673d3
Krishnandan Sharma <ksharma99056@gmail.com>
2020-10-28 18:40:24 +0530

    Initial Project Setup

---
+++ uber-backend-api/src/main/java/com/freelearningapp/uber/UberBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.freelearningapp.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBackendApiApplication.class, args);
+	}
+
+}
---
+++ uber-backend-api/src/test/java/com/freelearningapp/uber/UberBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelearningapp.uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
